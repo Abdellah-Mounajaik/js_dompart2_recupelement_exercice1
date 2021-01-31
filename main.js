01ad2c06de736575f71d65ca06d668992048ed1c
Abdellah <mounajaikabdellah@gmail.com>
2021-01-31 19:38:43 +0100

    exo_fini

---
+++ main.js
@@ -0,0 +1,6 @@
+let tartine = document.getElementsByClassName("tartine")[0].innerText
+console.log(tartine);
+let toast = document.getElementById("toast").innerText
+console.log(toast);
+derniereClass = document.getElementsByClassName("tartine")[1].innerText
+console.log(derniereClass);
